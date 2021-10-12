b1cd6fef7602e68c67b498e4d56958e46d8b3179
ToshitaC <toshitachaudhari17@gmail.com>
2021-10-12 10:30:36 +0530

    Added:Repetition Practice Problems with for loop

---
+++ Day38/ForLoop/PrimeNum.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+let num = Number(prompt('Enter the number: '));
+let flag = true;
+if (num <= 1)
+    flag = false;
+else {
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0)
+            flag = false;
+    }
+}
+if (flag == false)
+    console.log("It is not a prime number");
+if (flag == true)
+    console.log("It is a prime number");
